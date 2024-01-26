3f9f4214747946ae93cae14ef2392c315739a23a
shubham oulkar <91728992+ShubhamOulkar@users.noreply.github.com>
2024-01-26 17:58:39 +0530

    re-writing calculateCurrentState() function

--- contracts/pass_proposal.sol
+++ contracts/pass_proposal.sol
@@ -92,18 +92,10 @@ contract ProposalContract {
         uint256 approve = proposal.approve;
         uint256 reject = proposal.reject;
         uint256 pass = proposal.pass;
+
+        pass = pass % 2 == 1 ? (pass+1)/2 : pass/2;
         
-        if (proposal.pass %2 == 1) {
-            pass += 1;
-        }
-
-        pass = pass / 2;
-
-        if (approve > reject + pass) {
-            return true;
-        } else {
-            return false;
-        }
+       return approve > (reject + pass) ? true : false;
     }
 
 
